cf25583f249fbc4cd2d7ae75649495358ee35e9b
KingDuddles <marcussauldudley@gmail.com>
2023-03-07 16:48:08 +1030

    Added button for user input to increase grid size

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-const container = document.querySelector('div');
+const container = document.querySelector('#container');
+const button = document.querySelector('#userGridSize');
 const sketchSize = 400;
-let gridSize = 16;
-// container.style.alignSelf = "center";
+let gridSize = 16; //set initial grid size
+let userInput;
+// add code to center the grid
 container.style.display = "flex";
 container.style.justifyContent = "center";
 container.style.alignItems = "stretch";
 container.style.flex = "1 1 0";
 container.style.width = sketchSize + "px";
 container.style.height = sketchSize + "px";
+container.style.margin = "auto";
 
-function createGrid() {
+createGrid(gridSize); //create initial grid
+
+button.addEventListener('click',() => {
+    userInput = prompt("Please enter your desired grid size up to 100");
+    if (userInput >= 1 && userInput <= 100){
+        removeGrid();
+        gridSize = userInput
+        createGrid(gridSize);
+    } else {
+        alert("Invalid input, please enter a number between 1 and 100");
+    }
+}) 
+
+function createGrid(gridSize) {
     for (let i = 0; i <gridSize; i++){
-        console.log(i);
         const cols = document.createElement('div');
         cols.style.display = "flex";
         cols.style.flexDirection = "column";
@@ -32,4 +47,13 @@ function createGrid() {
         }
     }
 }
+
+function removeGrid() {
+    // Function to remove existing grid
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
+
+
 
